b30180ba54223ce498ad610f1b898cc2f68d292e
Tushar Dev Singh <139161899+TusharRajputt@users.noreply.github.com>
2025-01-21 22:35:21 +0530

    Update video player and AI summary section in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -238,7 +238,7 @@ function App() {
               <iframe 
                 width="100%" 
                 height="100%" 
-                src={`https://www.youtube.com/embed/dQw4w9WgXcQ`}
+                src={`http://www.youtube.com/embed/dQw4w9WgXcQ`}
                 title="YouTube video player" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                 allowFullScreen
